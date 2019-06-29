8d89ec43f284c568fa9e3953cb6fef310980fc8c
ckallum <in18536@bristol.ac.uk>
2019-06-29 18:14:11 +0100

    Creating foundations

--- Tetris.py
+++ Tetris.py
@@ -1,46 +1,213 @@
 import pygame
+from random import choice
 
-pygame.init()
-window = pygame.display.set_mode((500, 500))
-pygame.display.set_caption('Tetris')
+dimensions = {'blockSize': 20, 'winHeight': 900, 'winWidth': 600, 'rows': 16, 'cols': 8}
 
-vel = 5
-width = 40
-height = 60
-posX = 0
-posY = 500 - height
-jump = 10
-isJump = False
+S1 = [['.....',
+       '......',
+       '..00..',
+       '.00...',
+       '.....'],
+      ['.....',
+       '..0..',
+       '..00.',
+       '...0.',
+       '.....']]
 
-run = True
+S2 = [['.....',
+       '.....',
+       '.00..',
+       '..00.',
+       '.....'],
+      ['.....',
+       '..0..',
+       '.00..',
+       '.0...',
+       '.....']]
 
-while run:
-    pygame.time.delay(100)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+S3 = [['..0..',
+       '..0..',
+       '..0..',
+       '..0..',
+       '.....'],
+      ['.....',
+       '0000.',
+       '.....',
+       '.....',
+       '.....']]
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and posX > vel:
-        posX -= vel
+S4 = [['.....',
+       '.....',
+       '.00..',
+       '.00..',
+       '.....']]
 
-    if keys[pygame.K_RIGHT] and posX < 500 - vel - width:
-        posX += vel
+S5 = [['.....',
+       '.0...',
+       '.000.',
+       '.....',
+       '.....'],
+      ['.....',
+       '..00.',
+       '..0..',
+       '..0..',
+       '.....'],
+      ['.....',
+       '.....',
+       '.000.',
+       '...0.',
+       '.....'],
+      ['.....',
+       '..0..',
+       '..0..',
+       '.00..',
+       '.....']]
 
-    if not isJump:
-        if keys[pygame.K_UP] and posY > vel:
-            posY -= vel
+S6 = [['.....',
+       '...0.',
+       '.000.',
+       '.....',
+       '.....'],
+      ['.....',
+       '..0..',
+       '..0..',
+       '..00.',
+       '.....'],
+      ['.....',
+       '.....',
+       '.000.',
+       '.0...',
+       '.....'],
+      ['.....',
+       '.00..',
+       '..0..',
+       '..0..',
+       '.....']]
 
-        if keys[pygame.K_DOWN] and posY < 500 - vel - height:
-            posY += vel
+S7 = [['.....',
+       '..0..',
+       '.000.',
+       '.....',
+       '.....'],
+      ['.....',
+       '..0..',
+       '..00.',
+       '..0..',
+       '.....'],
+      ['.....',
+       '.....',
+       '.000.',
+       '..0..',
+       '.....'],
+      ['.....',
+       '..0..',
+       '.00..',
+       '..0..',
+       '.....']]
 
-        if keys[pygame.K_SPACE]:
-            isJump = True
-    else:
-        pass
+shapes = [S1, S2, S3, S4, S5, S6, S7]
+colours = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
-    window.fill((0, 0, 0))
-    pygame.draw.rect(window, (255, 0, 0), (posX, posY, width, height))
-    pygame.display.update()
 
-pygame.quit()
+class Shape(object):
+    def __init__(self, column, row, shape):
+        self.x = column
+        self.y = row
+        self.shape = shape
+        self.colour = colours[shapes.index(shape)]
+        self.rotation = 0
+        self.size = len(shape)
+
+    def changeRotation(self):
+        self.rotation = (self.rotation + 1) % self.size
+
+    def getShape(self):
+        return self.shape
+
+
+def collision(shape, grid, occupied):
+    pass
+
+
+def clearLine(grid, occupied):
+    pass
+
+
+def getNextShape(current):
+    possible = list(filter(lambda x: x != current.getShape(), shapes))
+    return choice(possible)
+
+
+def drawShape(surface, shape):
+    pass
+
+
+def createGrid(occupied={}):
+    pass
+
+
+def drawGame(surface, rows, cols):
+    pass
+
+
+def gameOver(grid):
+    pass
+
+
+def drawNextShape(shape, surface):
+    pass
+
+
+class Tetris(object):
+    def __init__(self):
+        self.width = 600
+        self.height = 900
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.grid = ''
+        self.cell = 10
+        self.posX = 0
+        self.posY = self.height - self.cell
+        self.vel = 10
+        self.currentShape = Shape(0, 0, choice(shapes))
+        self.nextShape = getNextShape(self.currentShape)
+        self.difficulty = 1
+
+    def run(self):
+        run = True
+        pygame.init()
+        pygame.display.set_caption('Tetris')
+
+        while run:
+            pygame.time.delay(100)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT] and self.posX > 1:
+                self.posX -= self.vel
+
+            if keys[pygame.K_RIGHT] and self.posX < self.width - self.cell:
+                self.posX += self.vel
+
+            if keys[pygame.K_UP]:
+                self.currentShape = self.nextShape
+                self.nextShape = getNextShape(self.currentShape)
+
+            if keys[pygame.K_DOWN] and self.posY < self.height - self.cell:
+                self.posY += self.vel
+
+            self.window.fill((0, 0, 0))
+            pygame.draw.rect(self.window, (255, 0, 0), (self.posX, self.posY, self.cell, self.cell))
+            pygame.display.update()
+
+        pygame.quit()
+
+
+def main():
+    applet = Tetris()
+    applet.run()
+
+
+if __name__ == '__main__':
+    main()
